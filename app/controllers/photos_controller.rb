275c80d71e2dda653877e99690626e29c8473d3d
tynamight <toledano@chicagobooth.edu>
2022-03-17 20:12:24 +0000

    CRUD done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -25,4 +25,37 @@ class PhotosController < ApplicationController
     #render({ :template => "photo_templates/toast.html.erb"})
     redirect_to("/photos")
   end
+
+  def post
+    user_input = params.fetch("input_image")
+    user_caption = params.fetch("input_caption")
+    user_owner = params.fetch("input_owner_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = user_input
+    a_new_photo.caption = user_caption
+    a_new_photo.owner_id = user_owner
+
+    a_new_photo.save
+
+    redirect_to("/photos/"+ a_new_photo.id.to_s)
+  end
+
+  def update
+    updated_image = params.fetch("input_image")
+    updated_cap = params.fetch("input_caption")
+    constant_id = params.fetch("constant_id")
+
+    matching_photos = Photo.where({ :id => constant_id})
+    the_photo = matching_photos.at(0)
+
+    the_photo.image = updated_image
+    the_photo.caption = updated_cap
+    the_photo.save
+
+   redirect_to("/photos/"+ the_photo.id.to_s)
+  end
+
 end
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   get("/photos/:path_photo", { :controller => "photos", :action => "details"})
 
   get("/delete_photo/:path_photo", { :controller => "photos", :action => "toast"})
+
+  get("/insert_photo_record", { :controller => "photos", :action => "post"})
+
+  get("/update_photo/:constant_id", { :controller => "photos", :action => "update"})
+
 end
