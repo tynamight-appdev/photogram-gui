e97677277033fec323d07a1a477325f5d8a32b4c
tynamight <toledano@chicagobooth.edu>
2022-03-17 14:48:02 +0000

    Everything but the CUD

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,15 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos
     render({ :template => "photo_templates/index.html.erb"})
   end
+
+  def details
+    photo_path = params.fetch("path_photo")
+    matching_photos = Photo.where({ :id => photo_path}) 
+    @post = matching_photos.at(0)
+
+    @list_of_comments = Comment.where({:id => @post.id})
+
+    render({ :template => "photo_templates/details.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index"})
 
+  get("/photos/:path_photo", { :controller => "photos", :action => "details"})
+
 end
